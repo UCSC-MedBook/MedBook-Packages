c97ae42b8dceaa41b25e25f8ad0b87d46b1b24be
chrisw <chrisw@soe.ucsc.edu>
2015-08-12 13:32:11 -0700

    use cola graph layout in place of d3js force layout

--- circlemap-graph/package.js
+++ circlemap-graph/package.js
@@ -13,12 +13,9 @@ Package.describe({
 Package.onUse(function(api) {
     api.versionsFrom('1.1.0.2');
 
-    // api.use(['jquery']);
-    // api.use('jquery');
     api.use("alisalaah:jquery-contextmenu");
 
     api.export('circleMapGraph', 'client');
-    api.export('u', 'client');
     api.addFiles('circlemap-graph.js');
 });
 
