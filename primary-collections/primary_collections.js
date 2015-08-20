ae12bf3eff9495b05a043e9e089633c5f5a27e77
Robert Baetsch <baertsch@eduroam-169-233-202-98.ucsc.edu>
2015-08-19 17:32:41 -0700

    add new fields in mutation

--- primary-collections/primary_collections.js
+++ primary-collections/primary_collections.js
@@ -208,21 +208,37 @@ var networkInteractionSchema = new SimpleSchema({
 // });
 
 var mutationSchema = new SimpleSchema({ // used in PatientReports, GeneReports
-  "gene_label": { type: String }, // Hugo symbol
+  "gene_label": { type: String },
   "gene_id": { type: String },
   "protein_change": { type: String, optional: true },
+  "mutation_type": { type: String }, // variant_classification for us
   "chromosome": { type: String },
   "start_position": { type: Number },
   "end_position": { type: Number },
-  "strand": { type: String },
   "reference_allele": { type: String },
   "variant_allele": { type: String },
-  "variant_classification": { type: String },
-  "variant_type": { type: String },
   "MA_FImpact": { type: String, optional: true },
-  "MA_FIS": { type: Number, optional: true }
-});
+  "MA_FIS": { type: Number, optional: true },
+  "allele_count": { type: Number, label:"Allele count in genotypes, for each ALT allele, in the same order as listed", optional:true },
+  "allele_frequency": { type: Number, decimal:true, label:"Allele frequency, for each ALT allele, in the same order as listed", optional:true },
+  "allele_number": { type: Number, label:"Number of unique alleles across all samples", optional:true },
+  "base_quality": { type: Number, decimal:true, label:"Overall average base quality", optional:true },
+  "read_depth": { type: Number, label:"Total read depth for all samples", optional:true },
+  "fraction_alt": { type: Number, decimal:true, label:"Overall fraction of reads supporting ALT", optional:true },
+  "indel_number": { type: Number, label:"Number of indels for all samples", optional:true },
+  "modification_base_changes": { type: String, label:"Modification base changes at this position", optional:true },
+  "modification_types": { type: String, label:"Modification types at this position", optional:true },
+  "sample_number": { type: Number, label:"Number of samples with data", optional:true },
+  "origin": { type: String, label:"Where the call originated from, the tumor DNA, RNA, or both", optional:true },
+  "strand_bias": { type: Number, decimal:true, label:"Overall strand bias", optional:true },
+  "somatic": { type: Boolean, label:"Indicates if record is a somatic mutation", optional:true },
+  "variant_status": { type: Number, label:"Variant status relative to non-adjacent Normal, 0=wildtype,1=germline,2=somatic,3=LOH,4=unknown,5=rnaEditing" , optional:true},
+  "reads_at_start": { type: Number, label:"Number of reads starting at this position across all samples", optional:true },
+  "reads_at_stop": { type: Number, label:"Number of reads stopping at this position across all samples", optional:true },
+  "variant_type": { type: String, label:"Variant type, can be SNP, INS or DEL", optional:true },
+  "effects": { type: [Object], label:"Predicted effects Effect ( Effect_Impact | Functional_Class | Codon_Change | Amino_Acid_change| Amino_Acid_length | Gene_Name | Transcript_BioType | Gene_Coding | Transcript_ID | Exon  | GenotypeNum [ | ERRORS | WARNINGS ] )" , optional:true }
 
+});
 
 //
 // declare the collections
