905328f8a73e9d9830590cb54f982cb5a2410d69
mokolodi1 <mokolodi1@gmail.com>
2015-10-01 17:04:11 -0700

    created medbook:vcf-js

---
+++ parse-vcf/vcf-js/test/perf.js
@@ -0,0 +1,22 @@
+var assert = require("assert"),
+    fs = require("fs"),
+    _ = require("underscore"),
+    vcf = require("../vcf.js"),
+    Benchmark = require('benchmark');
+
+
+var suite = new Benchmark.Suite;
+
+var vcfText = fs.readFileSync('test/data/snv.vcf', {encoding: 'utf8'});
+
+suite
+.add('parse a small VCF', function() {
+  var data = vcf.parser()(vcfText);
+})
+.on('cycle', function(event) {
+  console.log(String(event.target));
+})
+.on('complete', function() {
+  console.log('Fastest is ' + this.filter('fastest').pluck('name'));
+})
+.run({'async': true});
