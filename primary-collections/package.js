9366abe6b9d525a1afc8b913fd3c65ff8b6f0f41
mokolodi1 <mokolodi1@gmail.com>
2015-11-16 16:44:42 -0800

    primary-collections@0.0.3

--- primary-collections/package.js
+++ primary-collections/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'medbook:primary-collections',
-  version: '0.0.2',
+  version: '0.0.3',
   // Brief, one-line summary of the package.
   summary: 'Primary collections and schemas for MedBook',
   // URL to the Git repository containing the source code for this package.
