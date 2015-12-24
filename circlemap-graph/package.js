9fb333c0366508025b605ca3d0cd944126513dde
chrisw <chrisw@soe.ucsc.edu>
2015-12-23 21:44:38 -0800

    circlemap-graph: v0.1.0 adds drug sensitivity

--- circlemap-graph/package.js
+++ circlemap-graph/package.js
@@ -1,6 +1,6 @@
 Package.describe({
 	name : 'medbook:circlemap-graph',
-    version : '0.0.1',
+    version : '0.1.0',
     // Brief, one-line summary of the package.
     summary : 'Draw a network graph with circlemaps as nodes.',
     // URL to the Git repository containing the source code for this package.
