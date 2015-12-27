37e121ee11f00cf6eb10ee3e66214a9773b42ead
mokolodi1 <mokolodi1@gmail.com>
2015-12-27 16:04:28 -0500

    some package incompatibilities

--- charts/package.js
+++ charts/package.js
@@ -2,18 +2,18 @@ Package.describe({
   name: 'medbook:charts',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'Charts for genomic research',
   // URL to the Git repository containing the source code for this package.
   git: '',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
-  documentation: 'README.md'
+  documentation: null
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
 
-  api.use('iron:router');
+  api.use('iron:router@1.0.9'); // TODO: do we really need this? 
 
   api.addFiles('helpers.js', 'client');
 
