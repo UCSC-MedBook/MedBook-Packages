4485569dfa6bfc7e0b9fd32bdc4d0dc2b8ed5a20
mokolodi1 <mokolodi1@gmail.com>
2015-12-27 15:51:38 -0500

    published report-collections to put on production

--- report-collections/package.js
+++ report-collections/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'medbook:report-collections',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'Report collections and schemas for MedBook',
   // URL to the Git repository containing the source code for this package.
