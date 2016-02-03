7b7f486833e9856f80bc75e31af3f32c6d69ef9d
chrisw <chrisw@soe.ucsc.edu>
2016-02-03 11:51:44 -0800

    select2genelist@0.1.12 - published with "meteor publish --release 1.1.0.2"

--- select2genelist/package.js
+++ select2genelist/package.js
@@ -1,15 +1,7 @@
 Package.describe({
     name: 'medbook:select2genelist',
-    version : '0.1.9',
-	// v0.1.9 - update package requirements
-	// v0.1.8 - publish with "meteor publish --release METEOR@1.1.0.2"
-	// v0.1.7 - update package requirements
-	// v0.1.6 - publish with "meteor publish --release METEOR@1.1.0.3"
-	// v0.1.5 - update package requirements
-	// v0.1.4 - update package requirements
-	// v0.1.3 - update package requirements
-	// v0.1.2 - update package requirements
-	// v0.1.1 - update package requirements
+    version : '0.1.12',
+	// v0.1.1-12 troubleshooting, finally published with "meteor publish --release 1.1.0.2"
     // v0.1.0 - Include the template into the package.
 	summary : 'A select2 widget that gives gene symbol suggestions via an included http method.',
     git : 'https://github.com/UCSC-MedBook/MedBook-Packages.git',
@@ -17,8 +9,8 @@ Package.describe({
 });
 
 Package.onUse(function(api) {
-    api.versionsFrom('METEOR@1.1.0.2');
-	api.use(["templating@1.0.0", 'cfs:http-methods@0.0.27', 'natestrauser:select2@4.0.0_1']);
+    api.versionsFrom('1.1.0.2');
+	api.use(["templating", 'cfs:http-methods@0.0.27', 'natestrauser:select2@4.0.0_1']);
 
     api.addFiles('http_methods.js', 'server');
     api.addFiles(['select2genelist.js', 'template.html', 'template.js'], 'client');
@@ -28,7 +20,6 @@ Package.onUse(function(api) {
 });
 
 Package.onTest(function(api) {
-    api.use('ecmascript');
     api.use('tinytest');
     // api.use('select2genelist');
     api.addFiles('select2genelist-tests.js');
