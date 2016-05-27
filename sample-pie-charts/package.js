6ed473c8b507fe0cd48fe497cb72b8b13c50c139
chrisw <chrisw@soe.ucsc.edu>
2016-05-27 16:15:33 -0700

    medbook:sample-pie-charts@0.0.2 - pie_charts object moved to its own package

--- sample-pie-charts/package.js
+++ sample-pie-charts/package.js
@@ -1,18 +1,20 @@
 Package.describe({
   name: "medbook:sample-pie-charts",
   summary: "Draw sample pie chart clinical data summaries.",
-  version: "0.0.1",
+  version: "0.0.2",
+  // 0.0.2 - pie_charts object moved to its own package
   git: "https://github.com/UCSC-MedBook/MedBook-Packages.git"
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('METEOR@0.9.3.1');
+//  api.versionsFrom('METEOR@0.9.3.1');
+  api.versionsFrom('1.1.0.2');
+  // to publish:
+  // delete .version and versions.json files
+  // "meteor publish --release 1.1.0.2"
   api.export('pie_charts', 'client');
-  api.addFiles('sample-pie-charts.js','client');
 });
 
 Package.onTest(function(api) {
   api.use('tinytest');
-  // api.use('sample-pie-charts');
-  api.addFiles('sample-pie-charts-tests.js');
 });
